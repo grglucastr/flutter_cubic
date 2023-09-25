bd2f0aa58eb6fe4a3f570cd25a98316acabdab25
= <george.bentes@gmail.com>
2023-09-25 08:38:12 -0300

    add: top menu and main title

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubic/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Home page"));
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Menu
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.menu, size: 30, color: Colors.black54),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(.5)),
+                )
+              ],
+            ),
+          ),
+
+          //Discover
+          const SizedBox(height: 40,),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+
+          //Tab bar
+          const SizedBox(height: 40,),
+
+        ],
+      ),
+    );
   }
 }
